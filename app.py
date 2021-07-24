d76b61046becd48315bff80ba8c5de0a9d69b2dc
[your <alessandrofigo24@gmail.com>
2021-07-24 13:52:46 +0700

    Added download yfinance data

--- app.py
+++ app.py
@@ -2,25 +2,17 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 
-from helpers import relative_return
+from helpers import relative_return, download_csv
 
 tickers = ('BTC-USD', 'ETH-USD', 'LTC-USD', 'DOGE-USD', 'XRP-USD', 'ADA-USD')
 cryptos = ('BTC', 'ETH', 'LTC', 'XRP', 'ADA', 'DOGE')
 currencies = ('USD', 'EUR', 'GBP')
 
-screen = st.sidebar.selectbox("Select Feature", ("Download Data", "Compare Returns", "Technicals"), index=1)
+screen = st.sidebar.selectbox("Select Feature", ("Download Data", "Compare Returns", "View Technicals", "Generate Predicitons"), index=1)
 
 st.title(screen)
 
 if screen == "Compare Returns":
-    
-    # crypto_col, currency_col = st.beta_columns(2)
-
-    # crypto = crypto_col.multiselect('Choose assets', cryptos, ('BTC'))
-    # currency = currency_col.selectbox('Choose currency', currencies)
-
-    # for i in range(len(crypto)):
-    #     crypto[i] = crypto[i] + '-' + currency
 
     crypto = st.multiselect('Choose assets', tickers, ('BTC-USD'))
 
@@ -35,4 +27,30 @@ if screen == "Compare Returns":
         st.line_chart(df)
 
 if screen == "Download Data":
+    
+    crypto_col, currency_col = st.beta_columns(2)
+
+    crypto = crypto_col.selectbox('Choose assets', cryptos)
+    currency = currency_col.selectbox('Choose currency', currencies)
+
+    ticker = crypto + '-' + currency
+
+    start = st.date_input('Start', value = pd.to_datetime('2021-01-01'))
+    end = st.date_input('End', value = pd.to_datetime('today'))
+
+    df_price = yf.download(ticker, start, end)
+
+    st.dataframe(df_price)
+    st.markdown(download_csv(df_price), unsafe_allow_html=True)
+
+if screen == "View Technicals":
+    # Turn indicators like RSI & BOLL on and off
+    st.write("In progress 🔨")
     pass
+
+if screen == "Generate Predicitons":
+    # Generate simple predicitons using prophet
+    st.write("In progress 🔨")
+    pass
+
+
--- helpers.py
+++ helpers.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import base64
 
 def relative_return(df):
     rel = df.pct_change()
     cumret = (1 + rel).cumprod() - 1
     cumret = cumret.fillna(0)
     return cumret
+
+def download_csv(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="price_history.csv">Download CSV File</a>'
+    return href
